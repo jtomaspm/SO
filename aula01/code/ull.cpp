e06e0d2aa82aa30ea95da4f9fab4666d2d503587
João Machado <jtomaspm@ua.pt>
2022-09-27 20:10:46 +0100

    redo

--- aula01/code/ull.cpp
+++ aula01/code/ull.cpp
@@ -2,127 +2,157 @@
  *
  * \author (2016) Artur Pereira <artur at ua.pt>
  */
-#include <errno.h>
-#include <stdint.h>
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
+#include <errno.h>
+#include <stdint.h>
 
 #include "ull.h"
 
-// My imports
-#include <iostream>
+namespace ull
+{
+  /* ************************************************* */
 
-namespace ull {
-/* ************************************************* */
+  /* The information support data structure  */
+  struct Register
+  {
+    uint32_t nmec;       //!< student number
+    const char *name;    //!< student name
+  };
 
-/* The information support data structure  */
-struct Register {
-  uint32_t nmec;    //!< student number
-  const char *name; //!< student name
-};
+  /* The linked-list support data structure */
+  struct Node 
+  {
+    Register reg;
+    struct Node *next;
+  };
 
-/* The linked-list support data structure */
-struct Node {
-  Register reg;
-  struct Node *next;
-};
+  static Node *head = NULL;
 
-static Node *head = NULL;
+  /* ************************************************* */
 
-/* ************************************************* */
-
-void reset() {
-  while (head->next) {
-    struct Node *p = head->next;
-    free(head);
-    head = p;
+  void reset()
+  {
+    head = NULL;
   }
-  free(head);
-  head = NULL;
-}
 
-/* ************************************************* */
+  /* ************************************************* */
 
-void load(const char *fname) {}
+  void load(const char *fname)
+  {
+    FILE *fp = fopen(fname, "r");
+    if(fp == NULL){
+      perror("Can't open file!");
+    }
+    else{
+      char *line = NULL;
+      size_t len = 0;
+      while(getline(&line, &len, fp) != -1){
+        uint32_t nmec;
+        char name[50];
+        sscanf(line, "%[^;];%u[^\n]", &name[0], &nmec);
+        insert(nmec, name);
+      }
+      free(line);
+      fclose(fp);
+      print();
+    }
 
-/* ************************************************* */
+  }
 
-void print() {
-  struct Node *temp_node = head;
-  if (!head) {
-    std::cout << "List is empty!\n" << std::endl;
-  } else {
-    while (temp_node) {
-      printf("%d,%s\n", temp_node->reg.nmec, temp_node->reg.name);
-      temp_node = temp_node->next;
+  /* ************************************************* */
+
+  void print()
+  {
+    struct Node *node_pointer = head;
+    if(!node_pointer){
+      printf("Empty List...");
+    }
+    else {
+      while(node_pointer){
+        printf("nmec: %d, nome: %s\n", node_pointer->reg.nmec, node_pointer->reg.name);
+        node_pointer = node_pointer->next;
+      }
     }
   }
-}
 
-/* ************************************************* */
+  /* ************************************************* */
 
-void insert(uint32_t nmec, const char *name) {
-  struct Node *temp_n = (struct Node *)malloc(sizeof(struct Node));
-  temp_n->reg.name = strdup(name);
-  temp_n->reg.nmec = nmec;
+  void insert(uint32_t nmec, const char *name)
+  {
+    struct Node *temp_n = (struct Node *)malloc(sizeof(struct Node));
+    temp_n->reg.name = name;
+    temp_n->reg.nmec = nmec;
 
-  struct Node *p1;
-  p1 = head;
-  do {
-    if (!head) {
-      head = temp_n;
-      head->next = NULL;
-      break;
-    } else {
-      if (temp_n->reg.name < p1->reg.name) {
-        temp_n->next = p1;
-        p1 = temp_n;
-        break;
-      } else if (p1->next == NULL) {
-        p1->next = temp_n;
-        temp_n->next = NULL;
-        break;
-      } else if (temp_n->reg.nmec < (p1->next)->reg.nmec) {
-        temp_n->next = p1->next;
-        p1->next = temp_n;
+    struct Node *p1;
+    p1 = head;
+    do
+    {
+      if (head == NULL)
+      {
+        head = temp_n;
+        head->next = NULL;
         break;
       }
+      else
+      {
+        if (temp_n->reg.nmec < p1->reg.nmec)
+        {
+          temp_n->next = p1;
+          p1 = temp_n;
+          break;
+        }
+        else if (p1->next == NULL)
+        {
+          p1->next = temp_n;
+          temp_n->next = NULL;
+          break;
+        }
+        else if (temp_n->reg.nmec < (p1->next)->reg.nmec)
+        {
+          temp_n->next = p1->next;
+          p1->next = temp_n;
+          break;
+        }
+        p1 = p1->next;
+      }
+    } while (p1 != NULL);
+  }
+
+  /* ************************************************* */
+
+  const char *query(uint32_t nmec)
+  {
+    struct Node *p1;
+    p1 = head;
+    while(p1 != NULL){
+      if(p1->reg.nmec==nmec)
+        return p1->reg.name;
       p1 = p1->next;
     }
-  } while (p1);
-}
-
-/* ************************************************* */
-
-const char *query(uint32_t nmec) {
-  struct Node *p1 = head;
-  while (p1) {
-    if (p1->reg.nmec == nmec) {
-      return p1->reg.name;
-    }
-    p1 = p1->next;
+    return NULL;
   }
-  return NULL;
-}
 
-/* ************************************************* */
+  /* ************************************************* */
 
-void remove(uint32_t nmec) {
-  struct Node *current, *previous;
-  current = head;
-  previous = NULL;
-  while (current) {
-    if (current->reg.nmec == nmec) {
-      previous->next = current->next;
-      free(current);
+  void remove(uint32_t nmec)
+  {
+    struct Node *p1, *p2;
+    p1 = head;
+    p2 = NULL;
+    while (p1 != NULL)
+    {
+      if (p1->reg.nmec == nmec){
+        p2->next = p1->next;
+        free(p1);
+      }
+      p2 = p1;
+      p1 = p1->next;
     }
-    previous = current;
-    current = current->next;
   }
+
+  /* ************************************************* */
+
 }
-
-/* ************************************************* */
-
-} // namespace ull
